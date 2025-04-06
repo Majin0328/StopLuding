ea6aa1fca8e524763be7b780c990e17c0e948782
adriansb <adriansb2702@gmail.com>
2025-04-06 17:06:35 -0600

    footer.php agregado, reutilizacion de codigo

--- evaluacion.php
+++ evaluacion.php
@@ -134,15 +134,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </main>
 
     <!-- Footer -->
-    <footer class="bg-dark py-4 mt-auto">
-        <div class="container px-5">
-            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                <div class="col-auto">
-                    <div class="small m-0" style="color:#FFFFFF;">StopLuding &copy; DERECHOS RESERVADOS 2025</div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include('footer.php'); ?>
 </body>
 
 </html>
