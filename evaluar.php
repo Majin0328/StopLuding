692edfa2eb274c8dfd2667e834f73f43021985ff
adriansb <adriansb2702@gmail.com>
2025-04-06 16:20:11 -0600

    modificaciones adrian, principalmente nav y cuestionario, asi mismo guarde en carpeta los no usados

--- evaluar.php
+++ evaluar.php
@@ -2,193 +2,171 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Evaluar</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="images/StopLudingLogoFavicon.png" />
-    <!-- Custom Google font-->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-        rel="stylesheet" />
-    <!-- Bootstrap icons-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
-    <link href="css/styles.css?v=2" rel="stylesheet" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Evaluar</title>
+  <!-- Favicon-->
+  <link rel="icon" type="image/x-icon" href="images/StopLudingLogoFavicon.png" />
+  <!-- Custom Google font-->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+    rel="stylesheet" />
+  <!-- Bootstrap icons-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="css/styles.css" rel="stylesheet" />
+  <link href="css/styles.css?v=2" rel="stylesheet" />
 
 </head>
 
 <body>
-      <!-- Imagen de fondo 1 -->
-<div class="fondo-imagen" style="position: absolute; top: 240px; left: 180px; width: 200px; height: 300px;">
-  <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
+  <!-- Imagen de fondo 1 -->
+  <div class="fondo-imagen" style="position: absolute; top: 240px; left: 180px; width: 200px; height: 300px;">
+    <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
 
-<!-- Imagen de fondo 2 -->
-<div class="fondo-imagen" style="position: absolute; top: 550px; left: 950px; width: 200px; height: 300px;">
-  <img src="images/PBVertical.png" alt="Imagen 2" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
+  <!-- Imagen de fondo 2 -->
+  <div class="fondo-imagen" style="position: absolute; top: 550px; left: 950px; width: 200px; height: 300px;">
+    <img src="images/PBVertical.png" alt="Imagen 2" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
 
   <!-- Imagen de fondo 3 -->
   <div class="fondo-imagen" style="position: absolute; top: 1100px; left: 180px; width: 200px; height: 300px;">
-  <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
- <!-- Imagen de fondo 4 -->
- <div class="fondo-imagen" style="position: absolute; top: 1500px; left: 950px; width: 200px; height: 300px;">
-  <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
+    <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+  <!-- Imagen de fondo 4 -->
+  <div class="fondo-imagen" style="position: absolute; top: 1500px; left: 950px; width: 200px; height: 300px;">
+    <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
 
-<!-- Imagen de fondo 5-->
-<div class="fondo-imagen" style="position: absolute; top: 1900px; left: 180px; width: 200px; height: 300px;">
-  <img src="images/PDVertical.png" alt="Imagen 2" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
+  <!-- Imagen de fondo 5-->
+  <div class="fondo-imagen" style="position: absolute; top: 1900px; left: 180px; width: 200px; height: 300px;">
+    <img src="images/PDVertical.png" alt="Imagen 2" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
 
   <!-- Imagen de fondo 6 -->
   <div class="fondo-imagen" style="position: absolute; top: 2200px; left: 950px; width: 200px; height: 300px;">
-  <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
-
-<!-- Imagen de fondo 7 -->
-<div class="fondo-imagen" style="position: absolute; top: 2600px; left: 180px; width: 200px; height: 300px;">
-  <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
-
-<!-- Imagen de fondo 8 -->
-<div class="fondo-imagen" style="position: absolute; top: 3000px; left: 950px; width: 200px; height: 300px;">
-  <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
-
-<!-- Imagen de fondo 9 -->
-<div class="fondo-imagen" style="position: absolute; top: 3400px; left: 180px; width: 200px; height: 300px;">
-  <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
-
-<!-- Imagen de fondo 10 -->
-<div class="fondo-imagen" style="position: absolute; top: 3800px; left: 950px; width: 200px; height: 300px;">
-  <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
-
-<!-- Imagen de fondo 11 -->
-<div class="fondo-imagen" style="position: absolute; top: 3900px; left: 180px; width: 200px; height: 300px;">
-  <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
-</div>
-    <main class="flex-shrink-0">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark py-3">
-            <div class="container px-5">
-            <a class="navbar-brand" href="index.html">
-                     <img src="images\STOPLUDINGHEADERnb.png" alt="StopLuding" style="height: 40px;">
-                </a>                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation"><span
-                        class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-                        <li class="nav-item"><a class="nav-link" href="indexprincipal.php" style="color:#FFFFFF;">Principal</a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="contenido.php" style="color:#FFFFFF;">Contenido</a></li>
-                        <li class="nav-item"><a class="nav-link" href="evaluar.php" style="color:#FFFFFF;">Evaluar</a></li>
-                          <li class="nav-item"><a class="nav-link" href="perfil.php" style="color:#FFFFFF;">Perfil</a>
-                        <li class="nav-item"><a class="nav-link" href="contact.php" style="color:#FFFFFF;">Contacto</a>
-                        <li class="nav-item"><a class="nav-link" href="index.php" style="color:#FFFFFF;">Cerrar Sesion</a></li>
-
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <header class="py-3">
-            <div class="container pb-5">
-                <div class="row gx-3 align-items-center">
-                    <div class="col-xxl-5">
-                        <!-- Header text content-->
-                        <div class="text-center text-xxl-start">
-
-                            <div class="fs-1 fw-light text-muted ">
-                                <a class="display-5 fw-bolder nav-link text-gradient d-inline" href="evaluar.php">Formulario de ayuda</a>
-                                <br>
-                                <div class="fs-3 fw-light text-muted ">
-                                    <p>Te invitamos a contestar el siguiente formulario :D</p>
-                                    <p>Con ello podremos entregarte un plan de trabajo en función de tus necesidades</p>
-                                
-                              
-          <br>                    
-         <div class="container d-flex justify-content-center align-items-center" style="height: 55vh;">
-    <div>
-    <div class="chat-container">
-
-    <div class="chat-row">
-    <img src="images\LUDI PNG\ludi-kong.png" class="personaje-bigimg" alt="personaje 1">
-    <div class="chat-bubble-big">No soy un experto pero al menos tengo una corbata.</div>
+    <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
   </div>
-</div>
+
+  <!-- Imagen de fondo 7 -->
+  <div class="fondo-imagen" style="position: absolute; top: 2600px; left: 180px; width: 200px; height: 300px;">
+    <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+
+  <!-- Imagen de fondo 8 -->
+  <div class="fondo-imagen" style="position: absolute; top: 3000px; left: 950px; width: 200px; height: 300px;">
+    <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+
+  <!-- Imagen de fondo 9 -->
+  <div class="fondo-imagen" style="position: absolute; top: 3400px; left: 180px; width: 200px; height: 300px;">
+    <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+
+  <!-- Imagen de fondo 10 -->
+  <div class="fondo-imagen" style="position: absolute; top: 3800px; left: 950px; width: 200px; height: 300px;">
+    <img src="images/PBVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+
+  <!-- Imagen de fondo 11 -->
+  <div class="fondo-imagen" style="position: absolute; top: 3900px; left: 180px; width: 200px; height: 300px;">
+    <img src="images/PDVertical.png" alt="Imagen 1" style="width: 100%; height: 100%; object-fit: contain;">
+  </div>
+  <main class="flex-shrink-0">
+    <!-- Navigation-->
+    <?php include('navbar.php'); ?>
+
+    <header class="py-3">
+      <div class="container pb-5">
+        <div class="row gx-3 align-items-center">
+          <div class="col-xxl-5">
+            <!-- Header text content-->
+            <div class="text-center text-xxl-start">
+
+              <div class="fs-1 fw-light text-muted ">
+                <a class="display-5 fw-bolder nav-link text-gradient d-inline" href="evaluar.php">Formulario de ayuda</a>
+                <br>
+                <div class="fs-3 fw-light text-muted ">
+                  <p>Te invitamos a contestar el siguiente formulario :D</p>
+                  <p>Con ello podremos entregarte un plan de trabajo en función de tus necesidades</p>
+
+
+                  <br>
+                  <div class="container d-flex justify-content-center align-items-center" style="height: 55vh;">
+                    <div>
+                      <div class="chat-container">
+
+                        <div class="chat-row">
+                          <img src="images\LUDI PNG\ludi-kong.png" class="personaje-bigimg" alt="personaje 1">
+                          <div class="chat-bubble-big">No soy un experto pero al menos tengo una corbata.</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    </header>
+    <div class="text-center text-xxl-start">
+
+      <div class="fs-3 fw-light text-muted ">
+        <a class="display-5 fw-bolder nav-link text-gradient d-inline"
+          href="https://www.youtube.com/watch?v=JIge_4fCHVI">Veamos como estas inicialmente...</a>
+        <br>
+        <h4>Este formulario nos permitira determinar desde que punto estamos empezando contigo para crear un plan personalizado.</h4>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf_Ag3uBF3X3IQ72hPt2H4W2DH6GPP2iv2uPikJFki8hTVN8g/viewform?embedded=true"
+          width="640" height="3769" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+
+
+        </p>
+      </div>
     </div>
-    </div>
-                                </div>
-                                </div>
-                </header>
-                <div class="text-center text-xxl-start">
 
-<div class="fs-3 fw-light text-muted ">
-    <a class="display-5 fw-bolder nav-link text-gradient d-inline"
-        href="https://www.youtube.com/watch?v=JIge_4fCHVI">Veamos como estas inicialmente...</a>
-    <br>
-    <h4>Este formulario nos permitira determinar desde que punto estamos empezando contigo para crear un plan personalizado.</h4>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf_Ag3uBF3X3IQ72hPt2H4W2DH6GPP2iv2uPikJFki8hTVN8g/viewform?embedded=true"
-         width="640" height="3769" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
-
-   
-    </p>
-</div>
-</div>
-
-                                <footer class="bg-dark py-4 mt-auto">
-    <div class="container px-5">
+    <footer class="bg-dark py-4 mt-auto">
+      <div class="container px-5">
         <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-            <!-- Columna 1: Redes sociales -->
-            <div class="col-12 col-sm-4 mb-4 mb-sm-0">
-                <h5 class="text-white">Síguenos</h5>
-                <ul class="list-unstyled">
-                    <li><a href="https://www.facebook.com" class="text-white" target="_blank"><i class="bi bi-facebook"></i> Facebook</a></li>
-                    <li><a href="https://www.twitter.com" class="text-white" target="_blank"><i class="bi bi-twitter"></i> Twitter</a></li>
-                    <li><a href="https://www.instagram.com" class="text-white" target="_blank"><i class="bi bi-instagram"></i> Instagram</a></li>
-                    <li><a href="https://www.linkedin.com" class="text-white" target="_blank"><i class="bi bi-linkedin"></i> LinkedIn</a></li>
-                </ul>
-            </div>
+          <!-- Columna 1: Redes sociales -->
+          <div class="col-12 col-sm-4 mb-4 mb-sm-0">
+            <h5 class="text-white">Síguenos</h5>
+            <ul class="list-unstyled">
+              <li><a href="https://www.facebook.com" class="text-white" target="_blank"><i class="bi bi-facebook"></i> Facebook</a></li>
+              <li><a href="https://www.twitter.com" class="text-white" target="_blank"><i class="bi bi-twitter"></i> Twitter</a></li>
+              <li><a href="https://www.instagram.com" class="text-white" target="_blank"><i class="bi bi-instagram"></i> Instagram</a></li>
+              <li><a href="https://www.linkedin.com" class="text-white" target="_blank"><i class="bi bi-linkedin"></i> LinkedIn</a></li>
+            </ul>
+          </div>
 
-            <!-- Columna 2: Enlaces de referencia -->
-            <div class="col-12 col-sm-4 mb-4 mb-sm-0">
-                <h5 class="text-white">Referencias</h5>
-                <ul class="list-unstyled">
-                    <li><a href="politica_privacidad.html" class="text-white">Política de Privacidad</a></li>
-                    <li><a href="terminos_condiciones.html" class="text-white">Términos y Condiciones</a></li>
-                    <li><a href="contacto.html" class="text-white">Contacto</a></li>
-                </ul>
-            </div>
+          <!-- Columna 2: Enlaces de referencia -->
+          <div class="col-12 col-sm-4 mb-4 mb-sm-0">
+            <h5 class="text-white">Referencias</h5>
+            <ul class="list-unstyled">
+              <li><a href="politica_privacidad.html" class="text-white">Política de Privacidad</a></li>
+              <li><a href="terminos_condiciones.html" class="text-white">Términos y Condiciones</a></li>
+              <li><a href="contacto.html" class="text-white">Contacto</a></li>
+            </ul>
+          </div>
 
-            <!-- Columna 3: Iconos de servicios -->
-            <div class="col-12 col-sm-4 mb-4 mb-sm-0">
-                <h5 class="text-white">Servicios</h5>
-                <ul class="list-unstyled">
-                    <li><a href="evaluar.html" class="text-white"><i class="bi bi-check-circle"></i> Evaluación</a></li>
-                    <li><a href="contenido.html" class="text-white"><i class="bi bi-journal-text"></i> Contenido</a></li>
-                    <li><a href="perfil.html" class="text-white"><i class="bi bi-person-circle"></i> Perfil</a></li>
-                </ul>
-            </div>
+          <!-- Columna 3: Iconos de servicios -->
+          <div class="col-12 col-sm-4 mb-4 mb-sm-0">
+            <h5 class="text-white">Servicios</h5>
+            <ul class="list-unstyled">
+              <li><a href="evaluar.html" class="text-white"><i class="bi bi-check-circle"></i> Evaluación</a></li>
+              <li><a href="contenido.html" class="text-white"><i class="bi bi-journal-text"></i> Contenido</a></li>
+              <li><a href="perfil.html" class="text-white"><i class="bi bi-person-circle"></i> Perfil</a></li>
+            </ul>
+          </div>
         </div>
         <div class="row mt-3">
-            <div class="col text-center">
-                <div class="small m-0 text-white">STOPLUDING &copy; DERECHOS RESERVADOS 2025</div>
-            </div>
+          <div class="col text-center">
+            <div class="small m-0 text-white">STOPLUDING &copy; DERECHOS RESERVADOS 2025</div>
+          </div>
         </div>
-    </div>
-</footer>
+      </div>
+    </footer>
 </body>
 
 </html>
